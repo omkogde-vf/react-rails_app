3ed7f7a6e8e639a1e40136dd9a735608c0c9ef07
Om Kogde <okogde@verticalfox.com>
2024-07-10 19:04:51 +0530

    used render-json for converting to json for including in http request

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,10 +27,10 @@ class BooksController < ApplicationController
     respond_to do |format|
       if @book.save
         format.html { redirect_to book_url(@book), notice: "Book was successfully created." }
-        format.json { render :show, status: :created, location: @book }
+        
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
+        
       end
     end
   end
@@ -40,10 +40,10 @@ class BooksController < ApplicationController
     respond_to do |format|
       if @book.update(book_params)
         format.html { redirect_to book_url(@book), notice: "Book was successfully updated." }
-        format.json { render :show, status: :ok, location: @book }
+       
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
+        
       end
     end
   end
@@ -54,7 +54,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
-      format.json { head :no_content }
+     
     end
   end
 
